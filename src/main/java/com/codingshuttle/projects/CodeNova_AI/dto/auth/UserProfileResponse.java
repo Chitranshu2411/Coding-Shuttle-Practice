ea2de0d8b72ca548add613c9409490826a7e6c3b
Chitranshu <chitranshu2411@gmail.com>
2026-05-12 23:53:25 +0530

    Add user profile response DTO
    Add user profile response DTO

---
+++ src/main/java/com/codingshuttle/projects/CodeNova_AI/dto/auth/UserProfileResponse.java
@@ -0,0 +1,8 @@
+    package com.codingshuttle.projects.CodeNova_AI.dto.auth;
+
+    public record UserProfileResponse(
+            Long id,
+            String username,
+            String name
+    ) {
+    }
