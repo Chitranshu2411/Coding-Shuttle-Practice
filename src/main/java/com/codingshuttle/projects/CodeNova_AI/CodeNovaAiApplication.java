8ce770ec00df00ea577f6f3f2b89511e5fbc1f36
Chitranshu <chitranshu2411@gmail.com>
2026-05-12 23:50:04 +0530

    Initialize CodeNova AI application configuration

---
+++ src/main/java/com/codingshuttle/projects/CodeNova_AI/CodeNovaAiApplication.java
@@ -0,0 +1,14 @@
+package com.codingshuttle.projects.CodeNova_AI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeNovaAiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CodeNovaAiApplication.class, args);
+	}
+
+}
