184407363a6bdbbc4f1347fd979df61521b67b49
Chitranshu <chitranshu2411@gmail.com>
2026-05-19 22:37:22 +0530

    add subscription plan entity
    add subscription plan entity

---
+++ src/main/java/com/codingshuttle/projects/CodeNova_AI/entity/Plan.java
@@ -0,0 +1,29 @@
+package com.codingshuttle.projects.CodeNova_AI.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+public class Plan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String name;
+
+    @Column(unique = true)
+    String stripePriceId;
+    Integer maxProjects;
+    Integer maxTokensPerDay;
+    Integer maxPreviews;  //max number of previews allowed per plan
+    Boolean unlimitedAi;  //unlimited access to LLM, ignore maxTokendPerDay if true
+
+    Boolean active;
+}
