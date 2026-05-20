a8fbe02d98d0303fcf615a1996fd794438078231
Chitranshu <chitranshu2411@gmail.com>
2026-05-20 23:36:23 +0530

    add ProjectRole enum
     add ProjectRole enum

---
+++ src/main/java/com/codingshuttle/projects/CodeNova_AI/enums/ProjectRole.java
@@ -0,0 +1,24 @@
+package com.codingshuttle.projects.CodeNova_AI.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Set;
+
+import static com.codingshuttle.projects.CodeNova_AI.enums.ProjectPermission.*;
+
+@RequiredArgsConstructor
+@Getter
+public enum ProjectRole {
+//    ADMIN,
+//    MEMBER,
+    EDITOR(Set.of(VIEW,EDIT,DELETE,VIEW_MEMBERS)),
+    VIEWER(Set.of(VIEW, VIEW_MEMBERS)),
+    OWNER(Set.of(VIEW,EDIT,DELETE,MANAGE_MEMBERS,VIEW_MEMBERS));
+
+    ProjectRole(ProjectPermission... permissions) {
+        this.permissions = Set.of(permissions);
+    }
+
+    private final Set<ProjectPermission> permissions;
+}
